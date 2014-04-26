86b544e7fb573b73993ac1d5ded46e2984746ec6
Sarah <sarahrozman@gwu.edu>
2014-04-26 19:54:49 -0400

    updated links

---
+++ public_html/editprofile.php
@@ -0,0 +1,171 @@
+<?php session_start(); //continue session
+    $currentUser=$_SESSION['username'];
+    $currentID=$_SESSION['currentUserID'];
+    if(!$currentID){header("Location: index.html");exit;}
+    //connect to database
+    $conn = mysql_connect("localhost","lrcusack","S3Cr3T.228");
+    if (!$conn)
+        {echo "Unable to connect to database: " . mysql_error(); 
+        exit;}
+
+    $db = mysql_select_db('lrcusack',$conn);
+    if (!$db)
+	{echo "Unable to find database: " . mysql_error();
+	exit;}
+	
+	$sql = "SELECT * FROM singer WHERE 
+                singer_id='$currentID';";
+    $result=mysql_query($sql);
+            if (!$result) 
+            {
+                $name = 'SOP Member'; 
+        	exit;
+            }
+    $singer = mysql_fetch_assoc($result);
+            extract($singer);
+    $name = $singer['singer_name']
+
+    ?> 
+    
+<!DOCTYPE html>
+<!-- saved from url=(0050)http://getbootstrap.com/examples/navbar-fixed-top/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://getbootstrap.com/docs-assets/ico/favicon.png">
+
+    <title>Singer Sign Up</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://getbootstrap.com/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://getbootstrap.com/examples/navbar-fixed-top/navbar-fixed-top.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body style="">
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="./index.html">Sons of Pitch</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+             <li><a href="aboutus.html">About us</a></li>
+           <li><a href="events.php">Performance Schedule</a></li>
+           <li><a href="./contactus.html">Contact us</a></li>
+           <li><a href="currentsons.php">Current Sons</a></li>
+              </ul>
+            </li>
+          </ul>
+          
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Congratulations!</h1>
+        <p><br>You're a Son now, well done! Fill out some information below so you can be featured on the website.</p>
+        <p>
+        <form action="signupprocess.php" method="post" enctype="multipart/form-data">
+			<div style="text-align: left;">
+				<br>
+				First Name <input name="fname" type="text"><br>
+				<br>
+				Last Name <input name="lname" type="text"><br>
+				<br>
+				Email Address<input name="email" type="text"><br>
+				<br>
+				Password <input name="pw" type="password"><br>
+				<br>
+				Graduation Year <select name="gyear">
+					<option value="2000">2000</option>
+					<option value="2001">2001</option>
+					<option value="2002">2002</option>
+					<option value="2003">2003</option>
+					<option value="2004">2004</option>
+					<option value="2005">2005</option>
+					<option value="2006">2006</option>
+					<option value="2007">2007</option>
+					<option value="2008">2008</option>
+					<option value="2009">2009</option>
+					<option value="2010">2010</option>
+					<option value="2011">2011</option>
+					<option value="2012">2012</option>
+					<option value="2013">2013</option>
+					<option value="2014">2014</option>
+					<option value="2015">2015</option>
+					<option value="2016">2016</option>
+					<option value="2017">2017</option>
+					<option value="2018">2018</option>
+					<option value="2019">2019</option>
+					<option value="2020">2020</option>
+					<option value="2021">2021</option>
+					<option value="2022">2022</option>
+					<option value="2023">2023</option>
+					</select><br>
+				<br>
+				Voice Part<select name="vpart">
+					<option value="Tenor">Tenor</option>
+					<option value="Baritone">Baritone</option>
+					<option value="Bass">Bass</option>					
+					</select><br>
+				<br>
+				Home Town <input name="htown" type="text"><br>
+				<br>
+				Profile Picture
+				<input type="file" name="file" id="file">
+				<br>
+
+				Profile (Tell us about yourself!) <br>
+				<textarea name="profiletext" id="profiletext" cols="30" rows="8"> </textarea>
+				<br><br>
+				<input value="Register" type="submit">
+			</div>
+		</form>
+        
+        
+        
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+
+	<!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="">Back to top</a></p>
+        <p>Sons of Pitch · <a href="singersignin.php">Singer Sign-in</a></p>
+      </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./SOP_files/jquery-1.10.2.min.js"></script>
+    <script src="./SOP_files/bootstrap.min.js"></script>
+  
+
+</body></html>
