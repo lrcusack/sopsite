d8d66bcfd51fa9a5bd11dc5755aae6b43b95ac2f
Sarah <sarahrozman@gwu.edu>
2014-04-26 20:12:31 -0400

    updated links

---
+++ public_html/eventstemplate.php
@@ -0,0 +1,174 @@
+
+<?php session_start(); //continue session
+    $currentUser=$_SESSION['username'];
+    $currentID=$_SESSION['currentUserID'];
+    if(!$currentID){header("Location: index.html");exit;}
+    //connect to database
+    $conn = mysql_connect("localhost","lrcusack","S3Cr3T.228");
+    if (!$conn)
+        {echo "Unable to connect to database: " . mysql_error(); 
+        exit;}
+
+    $db = mysql_select_db('lrcusack',$conn);
+    if (!$db)
+	{echo "Unable to find database: " . mysql_error();
+	exit;}
+	
+	$sql = "SELECT * FROM singer WHERE 
+                singer_id='$currentID';";
+    $result=mysql_query($sql);
+            if (!$result) 
+            {
+                $name = 'SOP Member'; 
+        	exit;
+            }
+    $singer = mysql_fetch_assoc($result);
+            extract($singer);
+    $name = $singer['singer_name']
+
+    ?>
+
+<!DOCTYPE html>
+<!-- saved from url=(0050)http://getbootstrap.com/examples/navbar-fixed-top/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://getbootstrap.com/docs-assets/ico/favicon.png">
+
+    <title>Sons of Pitch</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://getbootstrap.com/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://getbootstrap.com/examples/navbar-fixed-top/navbar-fixed-top.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body style="">
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="./index.html">Sons of Pitch</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+             <li><a href="aboutus.html">About us</a></li>
+           <li><a href="events.php">Performance Schedule</a></li>
+           <li><a href="./contactus.html">Contact us</a></li>
+           <li><a href="currentsons.php">Current Sons</a></li>
+              </ul>
+            </li>
+          </ul>
+          
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Upcoming Events</h1>
+        
+        <p><br>Be sure to come out to these SOP performances!</p>
+        <p>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					Event Name: .......
+					<br>
+					Date: ##/####
+					<br>
+					Time: ##:##
+					<br>
+					Description: .........................
+					
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					Event Name: .......
+					<br>
+					Date: ##/####
+					<br>
+					Time: ##:##
+					<br>
+					Description: .........................
+					
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					Event Name: .......
+					<br>
+					Date: ##/####
+					<br>
+					Time: ##:##
+					<br>
+					Description: .........................
+					
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					Event Name: .......
+					<br>
+					Date: ##/####
+					<br>
+					Time: ##:##
+					<br>
+					Description: .........................
+					
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					Event Name: .......
+					<br>
+					Date: ##/####
+					<br>
+					Time: ##:##
+					<br>
+					Description: .........................
+					
+				</div>
+			</div>
+		</p>
+      </div>
+
+    </div> <!-- /container -->
+
+	<!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="">Back to top</a></p>
+        <p>Sons of Pitch · <a href="singersignin.php">Singer Sign-in</a></p>
+      </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./SOP_files/jquery-1.10.2.min.js"></script>
+    <script src="./SOP_files/bootstrap.min.js"></script>
+  
+
+</body></html>
---
+++ public_html/index.php
@@ -0,0 +1,117 @@
+
+<?php session_start(); //continue session
+    $currentUser=$_SESSION['username'];
+    $currentID=$_SESSION['currentUserID'];
+    if(!$currentID){header("Location: index.html");exit;}
+    //connect to database
+    $conn = mysql_connect("localhost","lrcusack","S3Cr3T.228");
+    if (!$conn)
+        {echo "Unable to connect to database: " . mysql_error(); 
+        exit;}
+
+    $db = mysql_select_db('lrcusack',$conn);
+    if (!$db)
+	{echo "Unable to find database: " . mysql_error();
+	exit;}
+	
+	$sql = "SELECT * FROM singer WHERE 
+                singer_id='$currentID';";
+    $result=mysql_query($sql);
+            if (!$result) 
+            {
+                $name = 'SOP Member'; 
+        	exit;
+            }
+    $singer = mysql_fetch_assoc($result);
+            extract($singer);
+    $name = $singer['singer_name']
+
+    ?>
+
+<!DOCTYPE html>
+<!-- saved from url=(0050)http://getbootstrap.com/examples/navbar-fixed-top/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://getbootstrap.com/docs-assets/ico/favicon.png">
+
+    <title>Sons of Pitch</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://getbootstrap.com/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://getbootstrap.com/examples/navbar-fixed-top/navbar-fixed-top.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body style="">
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="./index.html">Sons of Pitch</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+             <li><a href="aboutus.html">About us</a></li>
+           <li><a href="events.php">Performance Schedule</a></li>
+           <li><a href="./contactus.html">Contact us</a></li>
+           <li><a href="currentsons.php">Current Sons</a></li>
+              </ul>
+            </li>
+          </ul>
+          
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Site under construction</h1>
+        <img src="./SOP_files/logo.jpg" alt="SOP Logo" class="center-block img-responsive">
+        <p><br>Remember that really bad website we had before? The one where the "new" members were people who graduated years ago? No? Lucky you...</p>
+        <p>Our most attractive and most talented (in every way) member Liam Cusack has decided to right the ship. Stay tuned and email him at lrcusack@gwmail.gwu.edu if you have any questions.</p>
+        <p>Quick warning, most of the links on this page go to the old website. Venture if you dare...</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="https://www.facebook.com/pages/Sons-of-Pitch/7985174993?sk=info" role="button">Check us out on the Facebook »</a>
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+
+	<!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="">Back to top</a></p>
+        <p>Sons of Pitch · <a href="singersignin.php">Singer Sign-in</a></p>
+      </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./SOP_files/jquery-1.10.2.min.js"></script>
+    <script src="./SOP_files/bootstrap.min.js"></script>
+  
+
+</body></html>
