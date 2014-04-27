0eb0b326cf50b2d61640d3a94cd28bb380ce3dc7
Sarah <sarahrozman@gwu.edu>
2014-04-26 20:13:39 -0400

    updated links

---
+++ public_html/singersignin.php
@@ -0,0 +1,126 @@
+
+<?php session_start(); //continue session
+    $currentUser=$_SESSION['username'];
+    $currentID=$_SESSION['currentUserID'];
+    if(!$currentID){header("Location: index.html");exit;}
+    //connect to database
+    $conn = mysql_connect("localhost","lrcusack","S3Cr3T.228");
+    if (!$conn)
+        {echo "Unable to connect to database: " . mysql_error(); 
+        exit;}
+
+    $db = mysql_select_db('lrcusack',$conn);
+    if (!$db)
+	{echo "Unable to find database: " . mysql_error();
+	exit;}
+	
+	$sql = "SELECT * FROM singer WHERE 
+                singer_id='$currentID';";
+    $result=mysql_query($sql);
+            if (!$result) 
+            {
+                $name = 'SOP Member'; 
+        	exit;
+            }
+    $singer = mysql_fetch_assoc($result);
+            extract($singer);
+    $name = $singer['singer_name']
+
+    ?>
+
+<!DOCTYPE html>
+<!-- saved from url=(0050)http://getbootstrap.com/examples/navbar-fixed-top/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://getbootstrap.com/docs-assets/ico/favicon.png">
+
+    <title>Singer Sign In</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://getbootstrap.com/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://getbootstrap.com/examples/navbar-fixed-top/navbar-fixed-top.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body style="">
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="./index.html">Sons of Pitch</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+        	<li><a href="aboutus.html">About us</a></li>
+           <li><a href="events.php">Performance Schedule</a></li>
+           <li><a href="./contactus.html">Contact us</a></li>
+           <li><a href="currentsons.php">Current Sons</a></li>
+              </ul>
+            </li>
+          </ul>
+          
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Welcome!</h1>
+        <p>Sign in here to access all the SOP website's cool new features
+        <br></p>
+		<p></p><br><form action="loginprocess.php" method="post">
+					<div style="text-align: center;">Username <input
+						name="username" type="text">&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp; Password <input name="pw"
+						type="password">&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp; &nbsp;<input value="Sign in"
+						type="submit"> 
+					</div>
+				</form></p>
+		<h2>New to SOP?</h2>
+		<p>
+			<a class="btn btn-lg btn-primary" href="singersignup.html" role="button">Sign up here! »</a>
+		</p>
+        <p></p>
+      </div>
+
+    </div> <!-- /container -->
+
+	<!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="">Back to top</a></p>
+        <p>Sons of Pitch · <a href="singersignin.php">Singer Sign-in</a></p>
+      </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./SOP_files/jquery-1.10.2.min.js"></script>
+    <script src="./SOP_files/bootstrap.min.js"></script>
+  
+
+</body></html>
