0c7c0e2afa7ec63d248761db314433920d77dd76
Sarah <sarahrozman@gwu.edu>
2014-04-26 20:00:27 -0400

    updated links

--- public_html/carousel.php
+++ public_html/carousel.php
@@ -1,3 +1,33 @@
+
+
+<?php session_start(); //continue session
+    $currentUser=$_SESSION['username'];
+    $currentID=$_SESSION['currentUserID'];
+    if(!$currentID){header("Location: index.html");exit;}
+    //connect to database
+    $conn = mysql_connect("localhost","lrcusack","S3Cr3T.228");
+    if (!$conn)
+        {echo "Unable to connect to database: " . mysql_error(); 
+        exit;}
+
+    $db = mysql_select_db('lrcusack',$conn);
+    if (!$db)
+	{echo "Unable to find database: " . mysql_error();
+	exit;}
+	
+	$sql = "SELECT * FROM singer WHERE 
+                singer_id='$currentID';";
+    $result=mysql_query($sql);
+            if (!$result) 
+            {
+                $name = 'SOP Member'; 
+        	exit;
+            }
+    $singer = mysql_fetch_assoc($result);
+            extract($singer);
+    $name = $singer['singer_name']
+
+    ?>
 <?php
 
 //create connection
